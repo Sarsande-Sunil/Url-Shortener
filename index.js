4c0e46dfc796c11844d7d357f9205fd39f74d92c
Sarsande-Sunil <93374701+Sarsande-Sunil@users.noreply.github.com>
2022-06-02 23:35:41 +0530

    complete url shortner

--- index.js
+++ index.js
@@ -1,27 +1,57 @@
 const expressss = require("express");
 const mongoose = require("mongoose");
+const ShortUrl = require("./Models/ShortUrl.model");
 const app = expressss();
 
 // connect a database
 mongoose.connect("mongodb://localhost:27017/urlShortner", {
     useNewUrlParser: true,
     useUnifiedTopology:true,
-},console.log("database connected"));
+});
 
 // set app here 
 app.set("view engine", "ejs");
-
+app.use(expressss.urlencoded({ extended: false }));
 // set url
 
 
 
-app.get("/", (req, res) => {
-    res.render("index");
+app.get("/", async (req, res) => {
+    try {
+        const shortUrls = await ShortUrl.find();
+        res.render("index", { shortUrls: shortUrls });
+    }
+    catch (error) {
+        console.log(error.message);
+    }
 });
 
+
 // connect data base
-app.post("/shortUrls", (req, res) => {
-    
+app.post("/shortUrls", async (req, res) => {
+    try {
+        await ShortUrl.create({ full: req.body.fullUrl });
+        res.redirect("/")
+    }
+    catch (error) {
+        console.log(error.message)
+    }
+});
+
+// get back our short and redirect through that
+app.get("/:shortUrl", async (req, res) => {
+    try {
+        const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+        if (shortUrl == null) return res.status(404);
+
+        shortUrl.clicks++;
+        shortUrl.save();
+
+        res.redirect(shortUrl.full);
+    }
+    catch (error) {
+        console.log(error);
+    }
 });
 
 
